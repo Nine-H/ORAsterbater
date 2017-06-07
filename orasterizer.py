9077dd0d6ab65c8ee0d2f7b8d92ff97360aeef7e
Nine-H <nine.gentooman@gmail.com>
2017-06-07 18:45:16 +1000

    layers more separate

--- orasterizer.py
+++ orasterizer.py
@@ -35,26 +35,28 @@ def for_image(ora):
     print (root.tag, root.attrib['h'], root.attrib['w'])
     create_new_buffer(int(root.attrib['h']),int(root.attrib['w']))
     for elem in root:
-        ora_tree(elem)
-    print ('woot woot get the loot')
+        ora_tree(elem, data[0], data[1])
     dump (data[1], data[0])
     
 def clean_up():
     os.system ('rm -rf '+TMP_DIR)
 
-def ora_tree(elem):
+def ora_tree(elem, directory, name):
     if elem.tag == 'layer':
+        print ('layer operations')
         print (elem.tag, elem.attrib['composite-op'], elem.attrib['name'], elem.attrib['opacity'], elem.attrib['src'], elem.attrib['visibility'], elem.attrib['x'], elem.attrib['y'])
         add_to_buffer(elem.attrib['src'])
     if elem.tag == 'stack':
+        print ('stack operations')
         if 'name' in elem.attrib:
-            print (elem.attrib['composite-op'], elem.attrib['name'], elem.attrib['opacity'], elem.attrib['visibility'])
+            print (elem.tag, elem.attrib['composite-op'], elem.attrib['name'], elem.attrib['opacity'], elem.attrib['visibility'])
             if elem.attrib['name'] == 'skip':
                 return
             global layer_group
+            dump(name, directory)
             layer_group = elem.attrib['name']
         for child in elem:
-            ora_tree(child)
+            ora_tree(child, directory, name)
 
 #FIXME: I probably shouldn't be abusing globals like this
 def create_new_buffer(height, width):
@@ -68,9 +70,16 @@ def add_to_buffer(filename):
     image_buffer = Image.alpha_composite(file, image_buffer)
 
 def dump(name,directory):
+    print ('woot woot get the loot')
     global layer_group
     global image_buffer
-    image_buffer.save('./'+OUTPUT_DIR+directory+"/"+name+'_'+layer_group+'.png')
+    try:
+        layer_group = "_"+layer_group
+    except:
+        layer_group = ""
+    print (OUTPUT_DIR+directory+"/"+name+'_'+layer_group+'.png')
+    image_buffer.save('./'+OUTPUT_DIR+directory+"/"+name+layer_group+'.png')
+    del layer_group
 
 #FIXME: I probably shouldn't be abusing the unix shell like this
 find = subprocess.run(['find', '-depth', '-name', '*.ora'], stdout=subprocess.PIPE)
